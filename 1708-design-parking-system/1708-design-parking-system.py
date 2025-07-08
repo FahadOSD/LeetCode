dd375804e97d9b79f00c6ad31de2ddbc010b5c20
Md Fahad Mir <128697899+FahadOSD@users.noreply.github.com>
2025-07-08 06:01:54 +0600

    Time: 19 ms (67.5%), Space: 12.8 MB (57.81%) - LeetHub

---
+++ 1708-design-parking-system/1708-design-parking-system.py
@@ -0,0 +1,28 @@
+class ParkingSystem(object):
+    
+    def __init__(self, big, medium, small):
+        self.big = big
+        self.medium = medium
+        self.small = small
+        
+
+    def addCar(self, carType):
+        if carType == 1:
+            if self.big == 0:
+                return False
+            else:
+                self.big  -= 1
+                return True
+        elif carType == 2:
+            if self.medium == 0:
+                return False
+            else:
+                self.medium  -= 1
+                return True
+        if carType == 3:
+            if self.small == 0:
+                return False
+            else:
+                self.small  -= 1
+                return True
+
